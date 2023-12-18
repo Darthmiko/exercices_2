5c60e2193ec86d1fb91bcc0cd7d5523a669c4c64
szychowskim <szychowskim@educ.college-montreal.qc.ca>
2023-12-18 11:47:41 -0500

    exercices decembre fin

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 # exercices 2
+import random
 class NPC:
 
-    def chiffe_aléatoire():
-        if
-            dé1 + dé2 + dé3 > dé4
-        elif
-            dé1 + dé3 + dé4 > dé2
-        elif
-            dé2 + dé3 + dé4 > dé1
-        elif
-            dé1 + dé2 + dé4 > dé3
+    def chiffre_aléatoire(self):
+        dé1 = random.randint(1,6)
+        dé2 = random.randint(1,6)
+        dé3 = random.randint(1,6)
+        dé4 = random.randint(1,6)
+        caractéristique = 0
+        if dé1 & dé2 & dé3 > dé4:
+            caractéristique = dé1 + dé2 + dé3
+        elif dé1 & dé3 & dé4 > dé2:
+            caractéristique = dé1 + dé4 + dé3
+        elif dé2 & dé3 & dé4 > dé1:
+            caractéristique = dé4 + dé2 + dé3
+        elif dé1 & dé2 & dé4 > dé3:
+            caractéristique = dé1 + dé2 + dé4
 
-    def __init__(self):
-        self.force =
-        self.agilité =
-        self.constitution =
-        self.intelligence =
-        self.sagesse =
-        self.charisme =
+        return caractéristique
 
-    def __init__(self):
+    def __init__(self, nom, race, profession):
+        self.force = self.chiffre_aléatoire()
+        self.agilité = self.chiffre_aléatoire()
+        self.constitution = self.chiffre_aléatoire()
+        self.intelligence = self.chiffre_aléatoire()
+        self.sagesse = self.chiffre_aléatoire()
+        self.charisme = self.chiffre_aléatoire()
         self.armure = random.randint(1,12)
         self.nom = nom
         self.race = race
@@ -47,14 +53,25 @@ class Kobold(NPC):
         pass
 
     def subir_dommage(self, dommage):
-        self.dommage = random.randint(1,6)
+        self.pv -= dommage-self.armure
 
 class Héros(NPC):
     def __init__(self):
         super.__init__()
 
     def attaquer(self, cible):
-        pass
+        attaque = random.randint(1,20)
+        if attaque == 20:
+            cible.subir_dommage(random.randint(1,8))
+        elif attaque == 1:
+            pass
+        elif attaque == random.randint(2,19):
+            if self.attaque >= cible.armure:
+                cible.subir_dommage()
+            elif self.attaque <= cible.armure:
+                pass
 
     def subir_dommage(self, dommage):
         self.dommage = random.randint(1, 6)
+
+#ca devrait marcher
